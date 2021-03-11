16a6689464e50e70c971186d32b651e97b576f47
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-03-11 15:16:01 +0600

    modified:   src/test/java/ru/netology/test/CardDeliveryOrderTest.java 	new file:   src/test/java/ru/netology/test/DateDelivery.java

--- src/test/java/ru/netology/test/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/test/CardDeliveryOrderTest.java
@@ -7,18 +7,15 @@ import static com.codeborne.selenide.Condition.visible;
 
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+
 
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
-    LocalDate date = LocalDate.now();
-    LocalDate dateDelivery = date.plusDays(5);
-    DateTimeFormatter formatters = DateTimeFormatter.ofPattern("d.MM.uuuu");
-    String dataText = dateDelivery.format(formatters);
+     DateDelivery dataText = new DateDelivery();
+
 
 
     @BeforeEach
@@ -29,14 +26,20 @@ public class CardDeliveryOrderTest {
 
     @Test
     void shouldTestAllFields() {
-        $$("[class=input__control]").get(0).setValue("Москва");
-        $$("[class=input__control]").get(1).sendKeys( Keys.CONTROL +"A",Keys.DELETE);
-        $$("[class=input__control]").get(1).setValue(String.valueOf(dataText));
-        $$("[class=input__control]").get(2).setValue("Иван Грозный");
-        $$("[class=input__control]").get(3).setValue("+79012345678");
+        String dataDelivery = dataText.ReturnDate(5);
+        $("[data-test-id=city] [placeholder='Город']").setValue("Москва");
+        $("[data-test-id=date] [placeholder='Дата встречи']").
+                sendKeys( Keys.CONTROL +"A",Keys.DELETE);
+        $("[data-test-id=date] [placeholder='Дата встречи']").
+                setValue(String.valueOf(dataDelivery));
+        $("[data-test-id=name] [name='name']").setValue("Иван Грозный");
+        $("[data-test-id=phone] [name='phone']").setValue("+79012345678");
         $("[class=checkbox__box]").click();
         $(withText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
+        $(withText("Встреча успешно забронирована на")).
+                shouldBe(visible, Duration.ofSeconds(15));
+        $(withText(dataDelivery)).
+                shouldBe(visible, Duration.ofSeconds(15));
 
 
 
---
+++ src/test/java/ru/netology/test/DateDelivery.java
@@ -0,0 +1,19 @@
+package ru.netology.test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateDelivery {
+    public String ReturnDate (int plusDays){
+        LocalDate date = LocalDate.now();
+        LocalDate dateDelivery = date.plusDays(plusDays);
+        DateTimeFormatter formatters = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dataText = dateDelivery.format(formatters);
+        return dataText;
+
+    }
+
+
+
+
+}
